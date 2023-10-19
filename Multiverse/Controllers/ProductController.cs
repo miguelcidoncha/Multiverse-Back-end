dd38111ca202f7b7af03f9a3e32f8613a00aebb0
Javiser <jascjav@gmail.com>
2023-10-19 13:05:00 +0200

    asd

--- Multiverse/Controllers/ProductController.cs
+++ Multiverse/Controllers/ProductController.cs
@@ -116,19 +116,17 @@ namespace Multiverse.Controllers
                     {
                         existingProductItem.image = updatedProductItem.image;
                     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    existingProductItem.type = updatedProductItem.type;
-=======
-                    
->>>>>>> Laura
-=======
 
                     existingProductItem.type = updatedProductItem.type;
 
                     
 
->>>>>>> origin/Laura
+
+                    existingProductItem.type = updatedProductItem.type;
+
+                    
+
+
                     existingProductItem.name = updatedProductItem.name;
                     existingProductItem.price = updatedProductItem.price;
                     existingProductItem.description = updatedProductItem.description;
